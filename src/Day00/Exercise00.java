9b0340a7d9cc3ca873fa7468fbdd0ce7e282deae
Alex-pixe <skremeone1@gmail.com>
2023-05-30 18:01:09 +0300

    Hello world!

--- src/Day00/Exercise00.java
+++
@@ -1,7 +0,0 @@
-package Day00;
-
-public class Exercise00 {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/Day00/Exercise00/Program.java
@@ -0,0 +1,7 @@
+package Day00.Exercise00;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
