6f848db2f247a5477362e82fc1324755f6bc4274
Alex-pixe <skremeone1@gmail.com>
2023-05-31 13:34:11 +0300

    Day00 Ex01 done

---
+++ src/Day00/Ex01/Program.java
@@ -0,0 +1,37 @@
+package Day00.Ex01;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 1) {
+            System.err.println("IllegalArgument");
+            System.exit(-1);
+        }
+        int sqRootN = getSqrt(n);
+        for (int i = 2; i <= sqRootN; i++) {
+            if (n % i == 0) {
+                System.out.println("false " + (i - 1));
+                return;
+            }
+        }
+        System.out.println("true " + sqRootN);
+    }
+
+    public static int getSqrt(int x) {
+        double y = x;
+        double z = (y + (x / y)) / 2;
+
+        while (getAbs(y - z) >= 0.1) {
+            y = z;
+            z = (y + (x / y)) / 2;
+        }
+        return (int)z;
+    }
+
+    public static double getAbs(double x) {
+        return (x < 0.0) ? -x : x;
+    }
+}
