1adc7bcc9bb9fe5319ec3f0b7f9410a4bc717e84
Alex-pixe <skremeone1@gmail.com>
2023-05-31 13:33:44 +0300

    Day00 Ex00 added constants varibles

--- src/Day00/Ex00/Program.java
+++ src/Day00/Ex00/Program.java
@@ -1,20 +1,21 @@
 package Day00.Ex00;
 
 public class Program {
+    private static final int TENTHLY = 10;
+    private static final int HUNDREDS = 100;
+    private static final int THOUSANDS = 1000;
+    private static final int TENS_OF_THOUSANDS = 10000;
+    private static final int HUNDREDS_OF_THOUSANDS = 100000;
+
     public static void main(String[] args) {
         int x = 479598;
         int res = 0;
-        res += x % 10;
-        x /= 10;
-        res += x % 10;
-        x /= 10;
-        res += x % 10;
-        x /= 10;
-        res += x % 10;
-        x /= 10;
-        res += x % 10;
-        x /= 10;
-        res += x % 10;
+        res += x % TENTHLY;
+        res += x / TENTHLY % TENTHLY;
+        res += x / HUNDREDS % TENTHLY;
+        res += x / THOUSANDS % TENTHLY;
+        res += x / TENS_OF_THOUSANDS % TENTHLY;
+        res += x / HUNDREDS_OF_THOUSANDS % TENTHLY;
         System.out.println(res);
     }
 }
