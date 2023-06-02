f1eca96baecb56fe608e041b0a82f4a519cb236c
Alex-pixe <skremeone1@gmail.com>
2023-06-02 13:18:39 +0300

    Day00 Ex00 change variable name

--- src/Day00/Ex00/Program.java
+++ src/Day00/Ex00/Program.java
@@ -8,14 +8,14 @@ public class Program {
     private static final int HUNDREDS_OF_THOUSANDS = 100000;
 
     public static void main(String[] args) {
-        int x = 479598;
+        int number = 479598;
         int res = 0;
-        res += x % TENTHLY;
-        res += x / TENTHLY % TENTHLY;
-        res += x / HUNDREDS % TENTHLY;
-        res += x / THOUSANDS % TENTHLY;
-        res += x / TENS_OF_THOUSANDS % TENTHLY;
-        res += x / HUNDREDS_OF_THOUSANDS % TENTHLY;
+        res += number % TENTHLY;
+        res += number / TENTHLY % TENTHLY;
+        res += number / HUNDREDS % TENTHLY;
+        res += number / THOUSANDS % TENTHLY;
+        res += number / TENS_OF_THOUSANDS % TENTHLY;
+        res += number / HUNDREDS_OF_THOUSANDS % TENTHLY;
         System.out.println(res);
     }
 }
