8433e89a7cf0386c0644ee34d9ce6b13b4b97210
Alex-pixe <skremeone1@gmail.com>
2023-06-03 18:07:58 +0300

    day01 ex02 done

---
+++ src/day01/ex02/Program.java
@@ -0,0 +1,22 @@
+package day01.ex02;
+
+public class Program {
+    private static final int TEST_SIZE = 15;
+
+    public static void main(String[] args) {
+        UsersList test = new UsersArrayList();
+        for (int i = 0; i < TEST_SIZE; i++) {
+            test.addUser(new User("test_" + i, TEST_SIZE + i));
+        }
+
+        for (int i = 0; i < test.getNumberOfUsers(); i++) {
+            System.out.println(test.getUserByIndex(i).toString());
+        }
+
+        try {
+            test.getUserById(-10);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
