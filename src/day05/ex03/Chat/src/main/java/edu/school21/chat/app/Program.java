ebdee3d87dd90878396b1d5b9e67c4ce9e47df9c
cbridget <coachbridget42@gmail.com>
2023-06-18 19:32:37 +0300

    day05 ex03 done

---
+++ src/day05/ex03/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -0,0 +1,38 @@
+package edu.school21.chat.app;
+
+import edu.school21.chat.repositories.*;
+import edu.school21.chat.models.*;
+import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import edu.school21.chat.models.Message;
+
+public class Program {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USERNAME = "cbridget";
+    private static final String DB_PASSWORD = "123";
+
+    public static void main(String[] args) {
+        MessagesRepository messagesRepository = new MessagesRepositoryJdbcImpl(getHikariDataSource());
+        Optional<Message> messageOptional = messagesRepository.findById(4l);
+        if (messageOptional.isPresent()) {
+            Message message = messageOptional.get();
+            message.setText("Bye");
+            message.setDate(null);
+            messagesRepository.update(message);
+        }
+    }
+
+    public static DataSource getHikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(DB_URL);
+        config.setUsername(DB_USERNAME);
+        config.setPassword(DB_PASSWORD);
+        return new HikariDataSource(config);
+    }
+}
---
+++ src/day05/ex03/Chat/src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS chat;
+
+DROP TABLE IF EXISTS chat.messages;
+DROP TABLE IF EXISTS chat.rooms;
+DROP TABLE IF EXISTS chat.users;
+
+CREATE TABLE IF NOT EXISTS chat.users (
+    id SERIAL PRIMARY KEY,
+    login text UNIQUE NOT NULL,
+    password text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS chat.rooms (
+    id SERIAL PRIMARY KEY,
+    chat_name text UNIQUE NOT NULL,
+    owner INT NOT NULL REFERENCES chat.users(id)
+);
+
+CREATE TABLE IF NOT EXISTS chat.messages (
+    id SERIAL PRIMARY KEY,
+    author INT NOT NULL REFERENCES chat.users(id),
+    room INT NOT NULL REFERENCES chat.rooms(id),
+    message text NOT NULL,
+    send_time timestamp
+);
