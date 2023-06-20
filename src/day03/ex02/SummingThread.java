6fe05037a45272d320707762702d6961513827be
cbridget <coachbridget42@gmail.com>
2023-06-20 19:43:27 +0300

    day03 ex02 small fix

--- src/day03/ex02/SummingThread.java
+++ src/day03/ex02/SummingThread.java
@@ -4,19 +4,21 @@ public class SummingThread extends Thread {
     private final int[] array;
     private final int index;
     private final int numThreads;
+    private final int addLenght;
     private static int sum = 0;
 
     public SummingThread(int[] array, int index, int numThreads) {
         this.array = array;
         this.index = index;
         this.numThreads = numThreads;
+        this.addLenght = ((array.length / numThreads + 1) * (numThreads - 1) < array.length) ? 1 : 0;
     }
 
     @Override
     public void run() {
         int threadSum = 0;
-        int start = (array.length / numThreads + 1) * index;
-        int end = (array.length / numThreads + 1) * (index + 1);
+        int start = (array.length / numThreads + addLenght) * index;
+        int end = (array.length / numThreads + addLenght) * (index + 1);
         if (index + 1 == numThreads && end != array.length) {
             end = array.length;
         }
