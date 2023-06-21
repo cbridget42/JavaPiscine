26b6200e615805f7fe748a125441ee4e849a1b69
cbridget <coachbridget42@gmail.com>
2023-06-21 14:29:03 +0300

    day06 ex00 done

--- src/day06/ex00/src/test/java/edu/school21/numbers/NumberWorkerTest.java
+++ src/day06/ex00/src/test/java/edu/school21/numbers/NumberWorkerTest.java
@@ -3,6 +3,7 @@ package edu.school21.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberWorkerTest {
@@ -14,4 +15,23 @@ public class NumberWorkerTest {
         Assertions.assertTrue(numWorker.isPrime(num));
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {6, 42, 381, 1366, 4816})
+    void isPrimeForNotPrimes(int num) {
+        Assertions.assertFalse(numWorker.isPrime(num));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 0, -1, -5, -42})
+    void isPrimeForIncorrectNumbers(int num) {
+        Throwable exception = Assertions.assertThrows(
+                NumberWorker.IllegalNumberException.class, () -> numWorker.isPrime(num));
+        Assertions.assertEquals("your number is less than 2", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", delimiter = ',')
+    void checkDigitsSum(int input, int expected) {
+        Assertions.assertEquals(expected, numWorker.digitsSum(input));
+    }
 }
