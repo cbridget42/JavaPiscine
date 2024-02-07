2f0ec9938ab0261c6a4312b4cb42c62a9a79d70f
Aleksandr Smirnov <skremeone1@gmail.com>
2024-02-07 19:34:32 +0300

    day09 ex00

---
+++ src/day09/ex00/Chat/ClientServer/src/main/java/edu/school21/sockets/app/Main.java
@@ -0,0 +1,20 @@
+package edu.school21.sockets.app;
+
+import edu.school21.sockets.client.Client;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length != 1 || !args[0].matches("--serverPort=\\d{4}")) {
+            throw new IllegalArgumentException("must be argument --serverPort=****");
+        }
+        Client client = new Client(Integer.parseInt(args[0].substring(13)));
+        try {
+            client.start();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
---
+++ src/day09/ex00/Chat/SocketServer/src/test/java/edu/school21/sockets/services/UsersServiceImplTest.java
@@ -0,0 +1,29 @@
+package edu.school21.sockets.services;
+
+import edu.school21.sockets.models.User;
+import edu.school21.sockets.repositories.UsersRepositoryImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class UsersServiceImplTest {
+
+    UsersService usersService;
+    @Mock
+    UsersRepositoryImpl mockUsersRepository;
+
+    @Test
+    void signUpTest() {
+        this.usersService = new UsersServiceImpl(mockUsersRepository, null);
+        User user = new User("42", "21");
+        Mockito.when(mockUsersRepository.findByUserName(Mockito.any()))
+                .thenReturn(Optional.of(user));
+        usersService.signUp(user);
+        Mockito.verify(mockUsersRepository, Mockito.times(1)).findByUserName(Mockito.any());
+    }
+}
